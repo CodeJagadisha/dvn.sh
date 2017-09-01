5ca516e22fe744363695dd2fe4a8540e7b73995b
A Pretty Cool Program <APrettyCoolProgram@users.noreply.github.com>
2017-09-01 14:38:42 -0400

    v00.90.170901
    * Initial release

--- dvn.sh
+++ dvn.sh
@@ -2,201 +2,189 @@
 set -e
 
 #      FILENAME: dvn.sh
-#       VERSION: 00.89.00
-#         BUILD: 170831
+#       VERSION: 00.90
+#         BUILD: 170901
 #   DESCRIPTION: Used to setup a dvn environment in Linux
 #       AUTHORS: Christopher Banwarth (development@aprettycoolprogram.com)
 #     COPYRIGHT: 2017 A Pretty Cool Program
 #       LICENSE: Apache License, Version 2.0 [http://www.apache.org/licenses/LICENSE-2.0]
 #     MORE INFO: http://aprettycoolprogram.com/dvn
 
-## This script will build a software development environment for a Debian linux based distribution. I recommend using
-## this script on a clean environment. For the most part everything will be automatic, but there are some spots that
-## will need user interaction.
+AddAptGetRepository() {
+    case "$1" in
+        "code")
+            sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+            curl -k https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+            sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+            ;;
+        *) echo "Cannot add repository for $1"
+            ,,
+    esac
+}
 
-dvnTemp="$HOME/.dvn/temp"
+BuildPackage() {
+    case "$1" in
+        "abc")
+            wget -P $dvnTemp http://homepages.cwi.nl/~steven/abc/implementations/abc.tar.gz
+            sudo tar -C /usr/local -xzf $dvnTemp/abc.tar.gz
+            echo "PATH=$PATH:/usr/local/ABC" >> .profile
+            ;;
+        "go")
+            wget -P $dvnTemp https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz
+            sudo tar -C /usr/local -xzf $dvnTemp/go1.9.linux-amd64.tar.gz
+            mkdir $HOME/go
+            echo "PATH=$PATH:/usr/local/go/bin" >> .profile
+            echo "export GOPATH=$HOME/go" >> .bash_profile
+            ;;
+        "lua")
+            InstallAptGetPackage libreadline-dev
+            curl -R -O http://www.lua.org/ftp/lua-5.3.4.tar.gz
+            mv lua-5.3.4.tar.gz $dvnTemp
+            cd $dvnTemp
+            tar zxf lua-5.3.4.tar.gz
+            cd lua-5.3.4
+            make linux test
+            make install
+            ;;
+        "rust")
+            curl https://sh.rustup.rs -sSf | sh
+            ;;
+        "swift")
+            InstallAptGetPackage clang libicu-dev
+            wget -P $dvnTemp https://swift.org/builds/swift-3.1.1-release/ubuntu1610/swift-3.1.1-RELEASE/swift-3.1.1-RELEASE-ubuntu16.10.tar.gz
+            tar xzf $dvnTemp/swift-3.1.1-RELEASE-ubuntu16.10.tar.gz -C $dvnTemp
+            mkdir $dvnLanguages/Swift
+            mv $dvnTemp/swift-3.1.1-RELEASE-ubuntu16.10/* $dvnLanguages/Swift/
+            echo "PATH=$dvnLanguages/Swift/usr/bin" >> .profile
+            ;;
+        *)
+            echo "Cannot add Virtual Machine tools for $1"
+            ,,
+    esac
+}
 
-InstallPackage()
-{
+InstallAptGetPackage() {
     for package in "$@"; do
-        sudo apt-get -y install $package | tee $dvnTemp/install-$package.log
+        sudo apt-get -y install $package | tee $dvnLogs/$package-install.log
     done
 }
 
-InstallPackageMinimal()
-{
+InstallAptGetPackageMinimal() {
     for package in "$@"; do
-        sudo apt-get -y install $package --no-install-recommends | tee $dvnTemp/install-$package.log
+        sudo apt-get -y install $package --no-install-recommends | tee $dvnLogs/$package-install.log
     done
 }
 
-InstallBackportPackage()
-{
+InstallPipPackage() {
     for package in "$@"; do
-        sudo apt-get -y install -t jessie-backports $package --no-install-recommends | tee $dvnTemp/install-$package.log
+        sudo pip3 install $package | tee $dvnLogs/$package-install.log
     done
 }
 
-PurgePackage()
-{
-    ## Purge package(s) via apt-get, and drop a history file.
+InstallVirtualMachineTools() {
+    case "$1" in
+        "virtualbox")
+            wget -P $dvnTemp http://download.virtualbox.org/virtualbox/5.1.26/VBoxGuestAdditions_5.1.26.iso
+            sudo mount $dvnTemp/VBoxGuestAdditions_5.1.26.iso /media/cdrom
+            sudo cp /media/cdrom/VBoxLinuxAdditions.run $dvnTemp
+            sudo $dvnTemp/VBoxLinuxAdditions.run
+            sudo usermod -a -G vboxsf crispy
+            sudo umount /media/cdrom
+            ;;
+        *)
+            echo "Cannot add Virtual Machine tools for $1"
+            ,,
+    esac
+}
+
+PurgeAptGetPackage() {
     for package in "$@"; do
-        if [[ ! -f $HOME/.crispy/history/PurgePackage.$package ]]; then
-            sudo apt-get -y purge $package | tee $dvnTemp/purge-$package.log
-        fi
+        sudo apt-get -y purge $package | tee $dvnLogs/$package-install.log
     done
 }
 
-Update()
-{
-    sudo apt-get update | tee -a $dvnTemp/update-aptget.log
+StartupItem() {
+    case "$1" in
+        "nginx")
+            sudo systemctl enable nginx
+            ;;
+        *)
+            echo "Cannot add startup item for $1"
+            ;;
+    esac
 }
 
-Upgrade()
-{
-    sudo apt-get upgrade | tee -a $dvnTemp/upgrade-aptget.log
-    sudo apt-get dist-upgrade | tee -a $dvnTemp/dist-upgrade-aptget.log
+UpdateAptGet() {
+    sudo apt-get -y update | tee $dvnLogs/aptget-update.log
 }
 
-Clean()
-{    
-    if [[ $1 == "deborphan" ]]; then
-        sudo deborphan | xargs sudo apt-get -y remove --purge | tee -a $dvnTemp/deborphan-01.log
-        sudo deborphan --guess-data | xargs sudo apt-get -y remove --purge | tee -a $dvnTemp/deborphan-02.log
-    fi
+UpgradeAptGet() {
+    sudo apt-get -y upgrade | tee $dvnLogs/aptget-upgrade.log
+    sudo apt-get -y dist-upgrade | tee $dvnLogs/aptget-dist-upgrade.log
+}
 
-    if [[ $1 == "directories" ]]; then
-        sudo rm -rf /usr/share/man/* | tee -a $dvnTemp/remove-manpages.log
-        sudo rm -rf /usr/share/doc/* | tee -a $dvnTemp/remove-docfiles.log
-    fi
-
-    if [[ $1 == "bleachbit" ]]; then
-        sudo bleachbit --clean apt.* \
-                               bash.* \
-                               deepscan.* \
-                               system.* | tee -a $dvnTemp/bleachbit.log
-    fi
-
-    if [[ $1 == "aptget" ]]; then
-        sudo apt-get autoremove | tee -a $dvnTemp/autoremove-aptget.log
-        sudo apt-get autoclean | tee -a $dvnTemp/autoclean-aptget.log
-        sudo apt-get clean | tee -a $dvnTemp/clean-aptget.log
-    fi
+CleanAptGet() {
+    sudo apt-get autoremove | tee $dvnLogs/aptget-autoremove.log
+    sudo apt-get -y autoclean | tee $dvnLogs/aptget-autoclean.log
+    sudo apt-get -y clean | tee $dvnLogs/aptget-clean.log
 }
 
 # MAIN
 
+# Store passed arguments.
+dvnArgs="$@"
+
+# Create required directories and $PATH entries.
+dvnTemp="$HOME/.dvn/temp"
 mkdir -p $dvnTemp
+dvnLogs=$HOME/.dvn/logs/$(date "+%Y%m%d")
+mkdir -p $dvnLogs
+dvnLanguages=$HOME/Languages
+mkdir -p $dvnLanguages
 
-if [[ "#@" =~ "minimal" ]]; then
-    PurgePackage dictionaries-common \
-                 eject \
-                 gettext-base \
-                 gnupg \
-                 ispell \
-                 laptop-detect \
-                 vim-common \
-                 wamerican
+if [[ "$dvnArgs" =~ "--standard" ]]; then
+    touch $HOME/.bash_profile
+    InstallAptGetPackage localepurge curl apt-transport-https
+    AddAptGetRepository code | tee $dvnLogs/code-add-repository.log
+    UpdateAptGet
+    InstallAptGetPackage build-essential linux-headers-$(uname -r) htop xorg
+    InstallAptGetPackageMinimal xfce4
+    InstallAptGetPackage tango-icon-theme xfce4-terminal code filezilla iceweasel pidgin nginx openjdk-8-jdk python python3 \
+                        python3-pip python3-matplotlib python3-scipy ruby rails
+    InstallPipPackage jupyter
+    BuildPackagePackage go | tee $dvnLogs/go-install.log
+    BuildPackagePackage lua | tee $dvnLogs/lua-install.log
+    BuildPackagePackage rust | tee $dvnLogs/rust-install.log
+    StartupItem nginx | tee $dvnLogs/configure-nginx.log
 fi
 
-# Prerequistes
-Update
-InstallPackageMinimal localepurge \
-                      build-essential \
-                      linux-headers-$(uname -r) \
-                      curl \
-                      apt-transport-https
-
-if [[ "#@" =~ "minimal" ]]; then
-    InstallPackageMinimal python \
-                          deborphan \
-                          bleachbit
+# VirtualBox Guest Additions (optional).
+if [[ "$dvnArgs" =~ "--virtualbox" ]]; then
+    InstallVirtualMachineTools virtualbox | tee $dvnLogs/vboxguestadditions-install.log
 fi
 
-# 816MB
-
-# Window managers
-InstallPackage xorg
-InstallPackageMinimal xfce4
-InstallPackage tango-icon-theme \
-               xfce4-terminal
-
-# GUI applications
-InstallPackage filezilla
-InstallPackageMinimal pidgin
-
-# Oracle VirtalBox Guest Additions
-if [[ "#@" =~ "virtualbox" ]]; then
-    wget -P $dvnTemp http://download.virtualbox.org/virtualbox/5.1.26/VBoxGuestAdditions_5.1.26.iso
-    sudo mount $dvnTemp/VBoxGuestAdditions_5.1.26.iso /media/cdrom
-    sudo cp /media/cdrom/VBoxLinuxAdditions.run $dvnTemp
-    sudo $dvnTemp/VBoxLinuxAdditions.run
-    sudo usermod -a -G vboxsf crispy
-    sudo umount /media/cdrom
-fi
-
-# Microsoft Visual Studio Code
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-curl -k https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-Update
-InstallPackageMinimal "code"
-
-# Nginx
-InstallPackageMinimal nginx
-sudo systemctl enable nginx
-
-# OpenJava JDK
-sudo sh -c 'echo "deb http://http.debian.net/debian jessie-backports main non-free contrib" >> /etc/apt/sources.list'
-InstallBackportPackage openjdk-8-jdk
-
-# Python3, pip, and Jupyter notebooks - minimal?
-InstallPackage python3 \
-               python3-pip \
-               python3-matplotlib \
-               python3-scipy
-sudo pip3 install --upgrade pip # need?
-sudo pip3 install jupyter
-
-if [[ "#@" =~ "kitchensink" ]]; then
-    # ABC
-    wget $HOME/Downloads/ABC http://homepages.cwi.nl/~steven/abc/implementations/abc.tar.gz | tee $dvnTemp/install-abc.log
-    sudo tar -C /usr/local -xzf $HOME/Downloads/ABC/abc.tar.gz | tee -a $dvnTemp/install-abc.log
-
+# Tested packages not included in the standard build (optional).
+if [[ "$dvnArgs" =~ "--kitchensink" ]]; then
     # Ada
-    InstallPackage gnat
-
+    InstallAptGetPackage gnat
+    # Agda
+    InstallAptGetPackage agda
     # Erlang
-    InstallPackage erlang
-
-    # Go
-    wget -P $dvnTemp https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz | tee $dvnTemp/install-go.log
-    sudo tar -C /usr/local -xzf $dvnTemp/go1.8.3.linux-amd64.tar.gz | tee -a $dvnTemp/install-go.log
-    echo "PATH=$PATH:/usr/local/go/bin" >> .profile | tee -a $dvnTemp/install-go.log
-
-    # Lua
-    curl -R -O http://www.lua.org/ftp/lua-5.3.4.tar.gz > $dvnTemp/lua-5.3.4.tar.gz | tee $dvnTemp/install-lua.log
-    tar $dvnTemp/lua-5.3.4.tar.gz zxf lua-5.3.4.tar.gz | tee -a $dvnTemp/install-lua.log
-    cd $dvnTemp/lua-5.3.4.tar.gz/lua-5.3.4
-    make linux test | tee -a $dvnTemp/install-lua.log
-
-    # Perl - installed w/build-essentials
-
-    # Python2
-    InstallPackageMinimal python2
-
-    # Rust
-    curl https://sh.rustup.rs -sSf | sh | tee $dvnTemp/install-rust.log
+    InstallAptGetPackage erlang
+    # Swift
+    BuildPackagePackage swift | tee $dvnLogs/rust-install.log
 fi
 
-PurgePackage "orage"
-
-if [[ "#@" =~ "minimal" ]]; then
-    Clean deborphan
-    Clean directories
-    Clean bleachbit
+# Experimental packages, use at your own risk (optional).
+if [[ "$dvnArgs" =~ "--experimental" ]]; then 
+    desc="code-goes-here"
+    #BuildPackage abc | tee $dvnLogs/abc-install.log ## [170901] This will install, but doesn't seem to work correctly.
 fi
 
-Clean aptget
+# Upgrade the system, cleanup apt-get, archive logfiles, and remove temporary files.
+UpgradeAptGet
+CleanAptGet
+gzip $dvnLogs/*.log
+rm -rf $dvnTemp/*
 
-#clean logs
+sudo reboot
